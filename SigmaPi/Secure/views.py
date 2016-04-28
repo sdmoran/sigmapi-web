08746582c2d2eb351d299b1df6643db372eac504
timmarschall <services.tim.marschall@outlook.com>
2016-04-27 21:27:52 -0400

    Added calendar links by user perm

--- SigmaPi/Secure/views.py
+++ SigmaPi/Secure/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+
+from Secure.models import CalendarKey
 
 @login_required
 def index(request):
-	""" View for the index landing page of the site """
-	context = {
-		'title': 'Sigma Pi - Secure',
-		'secure_index': True,
-		'calendar_url': get_url(request)
-	}
-	return render(request,'secure_home.html',context)
+    """ View for the index landing page of the site """
+    context = {
+        'title': 'Sigma Pi - Secure',
+        'secure_index': True,
+        'calendar_url': get_url(request)
+    }
+    return render(request,'secure_home.html',context)
 
 def get_url(request):
-	groups = request.user.groups
+    groups = request.user.groups.all()
 
-	url = None
-	if (len(groups.filter(name="Philanthropy Chair")) > 0):
-		url = "https://teamup.com/ksa354443e2fdb07ef?view=d&sidepanel=c"
-	elif (len(groups.filter(name="Social Chair")) > 0):
-		url = "https://teamup.com/ks8866f0514e5d6a2f?view=d&sidepanel=c"
+    for group in groups:
+        if len(CalendarKey.objects.filter(group=group)) > 0:
+            cal_key = CalendarKey.objects.get(group=group)
+            return "https://teamup.com/%s?view=d&sidepanel=c" % cal_key.key
 
-	return url;
+    return False
