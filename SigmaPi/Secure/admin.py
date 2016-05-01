d5df9e50701f97749470febad07d8cca358704b0
Austin Rose <austin@percipientnetworks.com>
2016-05-01 01:47:30 -0400

    Keep keys in database.

---
+++ SigmaPi/Secure/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Secure.models import CalendarKey
+
+# Register models to appear in the Django Admin DB Site
+admin.site.register(CalendarKey)
---
+++ SigmaPi/Secure/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import Group
+
+class CalendarKey(models.Model):
+
+    # The group which has access to this key.
+    group = models.ForeignKey(Group, related_name="calendar_key", default=1)
+
+    # The calendar key.
+    key = models.CharField(max_length=100)
+
