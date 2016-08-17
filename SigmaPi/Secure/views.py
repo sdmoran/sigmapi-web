e2ce6ddac021f2653b7994b8f288cf15f7d07303
Austin Rose <austin.rose94@gmail.com>
2016-08-17 10:38:12 -0400

    Don't throw an exception in dev without calendar API keys.

--- SigmaPi/Secure/views.py
+++ SigmaPi/Secure/views.py
@@ -7,13 +7,15 @@ from Secure.models import CalendarKey
 @login_required
 def index(request):
     """ View for the index landing page of the site """
+
     context = {
         'title': 'Sigma Pi - Secure',
         'secure_index': True,
         'calendar_url': get_special_url(request),
         'general_calendar_url': get_general_url()
     }
-    return render(request,'secure_home.html',context)
+
+    return render(request, 'secure_home.html', context)
 
 def get_special_url(request):
     groups = request.user.groups.exclude(name="Brothers")
@@ -26,6 +28,9 @@ def get_special_url(request):
     return False
 
 def get_general_url():
-    brothers = Group.objects.get(name="Brothers")
-    cal_key = CalendarKey.objects.get(group=brothers)
-    return "https://teamup.com/%s?view=d&sidepanel=c" % cal_key.key
+    try:
+        brothers = Group.objects.get(name="Brothers")
+        cal_key = CalendarKey.objects.get(group=brothers)
+        return "https://teamup.com/%s?view=d&sidepanel=c" % cal_key.key
+    except Group.DoesNotExist, CalendarKey.DoesNotExist:
+        return False
