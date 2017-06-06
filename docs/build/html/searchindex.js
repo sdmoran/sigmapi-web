2f5cb6d16f57c36b1d62f53aff472714a5096571
Kyle McCormick <kylemccor@gmail.com>
2017-06-05 21:53:05 -0400

    Add docs/build

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Sigma Pi, Gamma Iota Chapter\u2019s documentation!"],titleterms:{chapter:0,document:0,gamma:0,indic:0,iota:0,sigma:0,tabl:0,welcom:0}})
