b123a383270fa725d5ec9c3cbaea2dae5c644dc5
Kyle McCormick <kylemccor@gmail.com>
2017-06-05 22:00:42 -0400

    Remove docs/build

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Sigma Pi, Gamma Iota Chapter\u2019s documentation!"],titleterms:{chapter:0,document:0,gamma:0,indic:0,iota:0,sigma:0,tabl:0,welcom:0}})
