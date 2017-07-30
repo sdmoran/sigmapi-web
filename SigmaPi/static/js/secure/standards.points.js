dcf4edba8e19994edb9bea38e75c1ea599c9b81e
Kyle McCormick <kylemccor@gmail.com>
2017-07-29 23:24:17 -0400

    Stndardize .js files ot use tabs

--- SigmaPi/static/js/secure/standards.points.js
+++ SigmaPi/static/js/secure/standards.points.js
@@ -3,11 +3,11 @@ var changes_table;
 var points_table;
 
 $(document).ready(function() {
-  setupClickListeners();
+	setupClickListeners();
 
-  requests_table = $("#requests-table").DataTable();
-  changes_table = $("#changes-table").DataTable();
-  points_table = $("#points-table").DataTable();
+	requests_table = $("#requests-table").DataTable();
+	changes_table = $("#changes-table").DataTable();
+	points_table = $("#points-table").DataTable();
 });
 
 /**
@@ -15,13 +15,13 @@ $(document).ready(function() {
  */
 function setupClickListeners()
 {
-  $("#add-brother-form-submit").click(submitAddBrotherForm);
-  $("#modify-points-form-submit").click(submitModifyPointsForm);
-  $(".approve-points-button").click(approveRequest);
-  $(".deny-request-button").click(denyPointsRequest);
+	$("#add-brother-form-submit").click(submitAddBrotherForm);
+	$("#modify-points-form-submit").click(submitModifyPointsForm);
+	$(".approve-points-button").click(approveRequest);
+	$(".deny-request-button").click(denyPointsRequest);
 
 
-  $("#points-table").on('click', 'a.modify-points-button', showModifyPointsForm);
+	$("#points-table").on('click', 'a.modify-points-button', showModifyPointsForm);
 }
 
 /**
@@ -29,13 +29,13 @@ function setupClickListeners()
  */
 function submitAddBrotherForm()
 {
-  var userid = $("#id_brother").val();
-  var points = $("#id_piPoints").val();
-  modifyPiPoints(userid, points);
+	var userid = $("#id_brother").val();
+	var points = $("#id_piPoints").val();
+	modifyPiPoints(userid, points);
 
-  $("#id_brother").val("");
-  $("#id_piPoints").val("");
-  $(this).modal("hide");
+	$("#id_brother").val("");
+	$("#id_piPoints").val("");
+	$(this).modal("hide");
 }
 
 /**
@@ -43,14 +43,14 @@ function submitAddBrotherForm()
  */
 function showModifyPointsForm()
 {
-  var id = $(this).attr("id"); // Retrieve user ID
-  var cur_points = $("#"+id+".points-points-field").first().text(); // Retrieve current points
-  var user = $("#"+id+".points-name-field").first().text(); // Retrieve username
+	var id = $(this).attr("id"); // Retrieve user ID
+	var cur_points = $("#"+id+".points-points-field").first().text(); // Retrieve current points
+	var user = $("#"+id+".points-name-field").first().text(); // Retrieve username
 
-  $("#modify-points-form-user").html(user); // Set username in form
-  $(".modify-points-form-userid").attr("id", id); // Set user ID in form
-  $("#modify-points-form-points").val(cur_points); // Set current points in form
-  $("#modify-points").modal("show"); // Finally display the form
+	$("#modify-points-form-user").html(user); // Set username in form
+	$(".modify-points-form-userid").attr("id", id); // Set user ID in form
+	$("#modify-points-form-points").val(cur_points); // Set current points in form
+	$("#modify-points").modal("show"); // Finally display the form
 }
 
 /**
@@ -58,11 +58,11 @@ function showModifyPointsForm()
  */
 function submitModifyPointsForm()
 {
-  var userid = $(".modify-points-form-userid").attr("id"); // Get user ID
-  var points = $("#modify-points-form-points").val(); // Get new points
-  modifyPiPoints(userid, points); // Send modify points request
+	var userid = $(".modify-points-form-userid").attr("id"); // Get user ID
+	var points = $("#modify-points-form-points").val(); // Get new points
+	modifyPiPoints(userid, points); // Send modify points request
 
-  $("#modify-points").modal("hide"); // Hide form
+	$("#modify-points").modal("hide"); // Hide form
 }
 
 /**
@@ -70,8 +70,8 @@ function submitModifyPointsForm()
  */
 function denyPointsRequest()
 {
-  var id = $(this).attr("id");
-  deleteRequest(id);
+	var id = $(this).attr("id");
+	deleteRequest(id);
 }
 
 /**
@@ -79,48 +79,48 @@ function denyPointsRequest()
  */
 function modifyPiPoints(userid, newpoints)
 {
-  var url = userid+"/";
-  var csrftoken = $.cookie('csrftoken');
-  $.ajax({
-    type:"POST",
-    beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-        // Send the token to same-origin, relative URLs only.
-        // Send the token only if the method warrants CSRF protection
-        // Using the CSRFToken value acquired earlier
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-     },
-    url: url,
-    data: {
-      "piPoints":newpoints,
-    },
-  }).done(function( data ) {
-    var id = data['id'];
-    var name = data['name'];
-    var oldPoints = data['old_points'];
-    var points = data['points'];
-    var date = data['date'];
-    var modifier = data['modifier'];
+	var url = userid+"/";
+	var csrftoken = $.cookie('csrftoken');
+	$.ajax({
+		type:"POST",
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+				// Send the token to same-origin, relative URLs only.
+				// Send the token only if the method warrants CSRF protection
+				// Using the CSRFToken value acquired earlier
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		 },
+		url: url,
+		data: {
+			"piPoints":newpoints,
+		},
+	}).done(function( data ) {
+		var id = data['id'];
+		var name = data['name'];
+		var oldPoints = data['old_points'];
+		var points = data['points'];
+		var date = data['date'];
+		var modifier = data['modifier'];
 
-    //Update the standings if it exists.
-    //Check if exists
-    if($("#"+id+".points-row").length > 0)
-    {
-      $("#"+id+".points-points-field").first().html(points);
-    }
-    else
-    {
-      //Otherwise create it.
-      var standings_data = [name, points, '<a id='+id+' class="btn btn-primary btn-sm modify-points-button">Modify Points</a>'];
-      points_table.fnAddData(standings_data);
-    }
+		//Update the standings if it exists.
+		//Check if exists
+		if($("#"+id+".points-row").length > 0)
+		{
+			$("#"+id+".points-points-field").first().html(points);
+		}
+		else
+		{
+			//Otherwise create it.
+			var standings_data = [name, points, '<a id='+id+' class="btn btn-primary btn-sm modify-points-button">Modify Points</a>'];
+			points_table.fnAddData(standings_data);
+		}
 
-    //Update the change log
-    var change_data = [date, modifier, name, oldPoints, points];
-    changes_table.fnAddData(change_data);
+		//Update the change log
+		var change_data = [date, modifier, name, oldPoints, points];
+		changes_table.fnAddData(change_data);
 
-  });
+	});
 }
 
 /**
@@ -128,51 +128,51 @@ function modifyPiPoints(userid, newpoints)
  */
 function approveRequest()
 {
-  var request_id = $(this).attr("id");
-  var url = "request/"+request_id+"/accept/";
-  var csrftoken = $.cookie('csrftoken');
-  $.ajax({
-    type:"POST",
-    beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-        // Send the token to same-origin, relative URLs only.
-        // Send the token only if the method warrants CSRF protection
-        // Using the CSRFToken value acquired earlier
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-     },
-    url: url,
-  }).done(function( data ) {
-    var pos = requests_table.fnGetPosition($("#"+request_id+".requests_row").get(0));
-    requests_table.fnDeleteRow(pos);
-    var id = data['id'];
-    var name = data['name'];
-    var oldPoints = data['old_points'];
-    var points = data['points'];
-    var date = data['date'];
-    var modifier = data['modifier'];
+	var request_id = $(this).attr("id");
+	var url = "request/"+request_id+"/accept/";
+	var csrftoken = $.cookie('csrftoken');
+	$.ajax({
+		type:"POST",
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+				// Send the token to same-origin, relative URLs only.
+				// Send the token only if the method warrants CSRF protection
+				// Using the CSRFToken value acquired earlier
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		 },
+		url: url,
+	}).done(function( data ) {
+		var pos = requests_table.fnGetPosition($("#"+request_id+".requests_row").get(0));
+		requests_table.fnDeleteRow(pos);
+		var id = data['id'];
+		var name = data['name'];
+		var oldPoints = data['old_points'];
+		var points = data['points'];
+		var date = data['date'];
+		var modifier = data['modifier'];
 
-    //Update the standings if it exists.
-    //Check if exists
-    if($("#"+id+".points-row").length > 0)
-    {
-      $("#"+id+".points-points-field").first().html(points);
-    }
-    else
-    {
-      //Otherwise create it.
-      var standings_data = [name, points, '<a id='+id+' class="ui tiny red button modify-points-button">Modify Points</a>'];
-      points_table.fnAddData(standings_data);
-    }
+		//Update the standings if it exists.
+		//Check if exists
+		if($("#"+id+".points-row").length > 0)
+		{
+			$("#"+id+".points-points-field").first().html(points);
+		}
+		else
+		{
+			//Otherwise create it.
+			var standings_data = [name, points, '<a id='+id+' class="ui tiny red button modify-points-button">Modify Points</a>'];
+			points_table.fnAddData(standings_data);
+		}
 
-    //Update the change log
-    var change_data = [date, modifier, name, oldPoints, points];
-    changes_table.fnAddData(change_data);
+		//Update the change log
+		var change_data = [date, modifier, name, oldPoints, points];
+		changes_table.fnAddData(change_data);
 
-    //Update the count
-    $("#pprCount").first().html(data.pprCount);
-    $("#pprCount2").first().html(data.pprCount);
-  });
+		//Update the count
+		$("#pprCount").first().html(data.pprCount);
+		$("#pprCount2").first().html(data.pprCount);
+	});
 }
 
 /**
@@ -180,25 +180,25 @@ function approveRequest()
  */
 function deleteRequest(requestid)
 {
-  var url = "request/"+requestid+"/delete/";
-  var csrftoken = $.cookie('csrftoken');
-  $.ajax({
-    type:"POST",
-    beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-        // Send the token to same-origin, relative URLs only.
-        // Send the token only if the method warrants CSRF protection
-        // Using the CSRFToken value acquired earlier
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-     },
-    url: url,
-  }).done(function( data ) {
-    var pos = requests_table.fnGetPosition($("#"+requestid+".requests_row").get(0));
-    requests_table.fnDeleteRow(pos);
-    $("#pprCount").first().html(data.pprCount);
-    $("#pprCount2").first().html(data.pprCount);
-  });
+	var url = "request/"+requestid+"/delete/";
+	var csrftoken = $.cookie('csrftoken');
+	$.ajax({
+		type:"POST",
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+				// Send the token to same-origin, relative URLs only.
+				// Send the token only if the method warrants CSRF protection
+				// Using the CSRFToken value acquired earlier
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		 },
+		url: url,
+	}).done(function( data ) {
+		var pos = requests_table.fnGetPosition($("#"+requestid+".requests_row").get(0));
+		requests_table.fnDeleteRow(pos);
+		$("#pprCount").first().html(data.pprCount);
+		$("#pprCount2").first().html(data.pprCount);
+	});
 }
 
 
@@ -207,19 +207,19 @@ function deleteRequest(requestid)
  ***********************************/
 
 function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+		// these HTTP methods do not require CSRF protection
+		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 function sameOrigin(url) {
-    // test that a given url is a same-origin URL
-    // url could be relative or scheme relative or absolute
-    var host = document.location.host; // host + port
-    var protocol = document.location.protocol;
-    var sr_origin = '//' + host;
-    var origin = protocol + sr_origin;
-    // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-        // or any other URL that isn't scheme relative or absolute i.e relative.
-        !(/^(\/\/|http:|https:).*/.test(url));
+		// test that a given url is a same-origin URL
+		// url could be relative or scheme relative or absolute
+		var host = document.location.host; // host + port
+		var protocol = document.location.protocol;
+		var sr_origin = '//' + host;
+		var origin = protocol + sr_origin;
+		// Allow absolute or scheme relative URLs to same origin
+		return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+				(url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+				// or any other URL that isn't scheme relative or absolute i.e relative.
+				!(/^(\/\/|http:|https:).*/.test(url));
 }
