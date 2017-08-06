63c754d5d48ed5257d215a789d54f26d337685d2
Kyle McCormick <kylemccor@gmail.com>
2017-08-06 19:50:07 -0400

    Upgrade to Python 3.6.2; comment install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
+# Add custom Python repo in order to get latest Python packages
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum update -y
+
+# Install Python development packages
 yum install -y gcc gcc-c++
-sudo yum install -y python35u python35u-libs python35u-devel python35u-pip
+sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
+pip3.6 install -r /vagrant/SigmaPi/requirements.txt
+
+# Install other utilities
 yum install -y git
-yum install -y nano
-pip3.5 install -r /vagrant/SigmaPi/requirements.txt
+yum install -y nano  # Learn vi noobs...
 
 # Create a python3 alias, and set default directory
-echo "alias python3=\"python3.5\"" >> .bashrc
+echo "alias python3=\"python3.6\"" >> .bashrc
 
 # Set the default directory when logging in
 echo "cd /vagrant/SigmaPi" >> .bash_profile
