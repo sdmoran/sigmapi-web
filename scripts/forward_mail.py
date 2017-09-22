57212252cb618149268eb52f43935015c620d3ac
Kyle McCormick <kylemccor@gmail.com>
2017-09-22 14:35:17 -0400

    Modify forward_mail to use env vars

--- scripts/forward_mail.py
+++ scripts/forward_mail.py
@@ -3,21 +3,26 @@
 Forward email to the web app.
 """
 import fileinput
+import os
 import requests
 
 
-_API_URL = (
-    'http://localhost:8000/secure/mailinglists/sendmail/'
-)
-
-
 def main():
+    """
+    Forward STDIN to the mailing lists sendmail API.
+    """
+    api_root = os.environ.get(
+        'MAILING_LISTS_API_ROOT',
+        'http://localhost:8000/',
+    )
+    api_key = os.environ.get('MAILING_LISTS_API_KEY')
     all_input = ''.join(line for line in fileinput.input())
     data = {
-        'key': None,
+        'key': api_key,
         'data': all_input,
     }
-    response = requests.post(_API_URL, data=data)
+    api_url = api_root + 'secure/mailinglists/sendmail/'
+    response = requests.post(api_url, data=data)
     return 0 if 200 < response.status_code < 299 else 1
 
 
