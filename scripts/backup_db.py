381dcba7ce87d876e962842d972f8114cba2da63
Kyle McCormick <kylemccor@gmail.com>
2017-09-30 22:48:18 -0400

    Improve backup script

--- scripts/backup_db.py
+++ scripts/backup_db.py
@@ -20,48 +20,55 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-u', '--user',
-        help='The user under which the database is stored. '
-             'Defaults to %s environment variable' % USER_ENVIRON
+        '-u',
+        '--user',
+        help=(
+            'The user under which the database is stored. '
+            'Defaults to {} environment variable'
+        ).format(USER_ENVIRON),
     )
     parser.add_argument(
-        '-n', '--name',
-        help='The name of the database. '
-             'Defaults to %s environment variable' % NAME_ENVIRON
+        '-n',
+        '--name',
+        help=(
+            'The name of the database. '
+            'Defaults to {} environment variable'
+        ).format(NAME_ENVIRON),
     )
     parser.add_argument(
-        '-p', '--port',
-        help='The port through which the database can be accessed.'
-             ' Defaults to %s environment variable' % PORT_ENVIRON
+        '-p',
+        '--port',
+        help=(
+            'The port through which the database can be accessed. '
+            'Defaults to {} environment variable'
+        ).format(PORT_ENVIRON),
     )
     parser.add_argument(
-        '-c', '--compress',
+        '-c',
+        '--compress',
         help='Flag indicating the backup should be compressed.',
-        action='store_true'
+        action='store_true',
     )
     parser.add_argument(
-        '-e', '--environment',
-        help='Flag indicating that the proper environment variables:\n'
-             '%s, %s, %s\n'
-             'should be set to the values used during this run.' % (NAME_ENVIRON, USER_ENVIRON, PORT_ENVIRON),
-        action='store_true'
+        '-e',
+        '--environment',
+        help=(
+            'Flag indicating that the proper environment variables:\n'
+            '({0}, {1}, {2})\n'
+            'should be set to the values used during this run.'
+        ).format(NAME_ENVIRON, USER_ENVIRON, PORT_ENVIRON),
+        action='store_true',
     )
     args = parser.parse_args()
     user, name, port = get_credentials(args)
-    if sys.stdout.isatty():
-        f_name = auto_name(name)
-    else:
-        f_name = None
-    if args.compress:
-        compress = True
-    else:
-        compress = False
+    f_name = auto_name(name) if sys.stdout.isatty() else None
+    compress = args.compress
     if args.environment:
         set_environment_vars(name, user, port)
     backup(user, name, port, f_name, compress)
 
 
-def backup(db_user, db_name, db_port, file_name=None, compressed=False):
+def backup(db_user, db_name, db_port=None, file_name=None, compressed=False):
     """
     Backs up the database.
 
@@ -72,7 +79,9 @@ def backup(db_user, db_name, db_port, file_name=None, compressed=False):
         file_name (str): Output file. Defaults to STDOUT.
         compressed (bool): If True, -Fc is used
     """
-    cmd = ['pg_dump', '-U', db_user, db_name, '-p', db_port]
+    cmd = ['pg_dump', '-U', db_user, db_name]
+    if db_port:
+        cmd += ['-p', db_port]
     if file_name:
         cmd += ['-f', file_name]
     if compressed:
@@ -89,18 +98,21 @@ def auto_name(db_name):
 
     Returns: str
     """
-    g_name = '%s_dump_%s' % (db_name, datetime.datetime.now())
+    g_name = '{0}_dump_{1}'.format(
+        db_name,
+        datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+    )
     if os.path.isfile(g_name):
         i = 1
-        while os.path.isfile(g_name + '(%d)' % i):
+        while os.path.isfile(g_name + '_{}'.format(i)):
             i += 1
-        g_name = g_name + '(%d)' % i
+        g_name = g_name + '_{}'.format(i)
     return g_name
 
 
 def get_credentials(cmd_args):
     """
-    Returns (username, database name, database port) if they do not exist in args.
+    Returns (username, database name, database port) if they're not in args.
 
     Checks for environment variables if needed, prompts the user if any
     information is missing.
@@ -120,7 +132,8 @@ def get_credentials(cmd_args):
             sys.stderr.write(
                 'No %s environment variable is set.'
                 'Either set the variable, or specify a user with -u.\n'
-                'Also be sure %s is set if not using -n flag and %s is set if not using -p flag.\n'
+                'Also be sure %s is set if not using -n flag and %s is set '
+                'if not using -p flag.\n'
                 % (USER_ENVIRON, NAME_ENVIRON, PORT_ENVIRON)
             )
             exit(-1)
@@ -140,18 +153,7 @@ def get_credentials(cmd_args):
             exit(-1)
     else:
         db_name = cmd_args.name
-    if not cmd_args.port:
-        try:
-            db_port = os.environ[PORT_ENVIRON]
-        except KeyError:
-            sys.stderr.write(
-                'No %s environment variable is set. Either set the variable, or specify a database with'
-                ' -n.\nAlso be sure %s is set if not using -u flag and %s is set if not using -n flag.\n'
-                % (PORT_ENVIRON, USER_ENVIRON, NAME_ENVIRON)
-            )
-            exit(-1)
-    else:
-        db_port = cmd_args.port
+    db_port = cmd_args.port or os.environ.get(PORT_ENVIRON)
     return db_user, db_name, db_port
 
 
