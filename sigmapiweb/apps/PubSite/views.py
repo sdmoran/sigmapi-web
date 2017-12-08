ca30d3947bb5adb4cfd7d8caf849ed8f80e0bf92
Kyle McCormick <kylemccor@gmail.com>
2017-12-08 16:48:32 -0500

    WIP: active page in navbar

--- sigmapiweb/apps/PubSite/views.py
+++ sigmapiweb/apps/PubSite/views.py
@@ -4,36 +4,60 @@ Views for PubSite app.
 from django.shortcuts import render
 
 
+_PAGES = {
+    'Home': '/',
+    'History': '/history',
+    'Service': '/service',
+    'Brothers':  '/brothers',
+    'Log In': '/secure',
+}
+
+
 def index(request):
     """
     View for the static index page
     """
-    return render(request, 'public/home2.html', None)
+    return render(
+        request, 'public/home2.html',
+        {'pages': _PAGES, 'current_page_name': 'Home'},
+    )
 
 
 def history(request):
     """
     View for the static chapter history page.
     """
-    return render(request, 'public/history.html', None)
+    return render(
+        request, 'public/history.html',
+        {'pages': _PAGES, 'current_page_name': 'History'},
+    )
 
 
 def service(request):
     """
     View for the static chapter service page.
     """
-    return render(request, 'public/service.html', None)
+    return render(
+        request, 'public/service.html',
+        {'pages': _PAGES, 'current_page_name': 'Service'},
+    )
 
 
 def donate(request):
     """
     View for the static donate page.
     """
-    return render(request, 'public/donate.html', None)
+    return render(
+        request, 'public/donate.html',
+        {'pages': _PAGES, 'current_page_name': 'Donate'},
+    )
 
 
 def permission_denied(request):
     """
     View for 403 (Permission Denied) error.
     """
-    return render(request, 'common/403.html', None)
+    return render(
+        request, 'common/403.html',
+        {'pages': _PAGES, 'current_page_name': 'Permission Denied'},
+    )
