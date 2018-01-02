06d36c9532b2efb853601374d3a39fb320a6d25c
co1in <colinjw3@gmail.com>
2018-01-02 17:28:44 -0500

    Made UserInfo searchable

--- sigmapiweb/apps/UserInfo/admin.py
+++ sigmapiweb/apps/UserInfo/admin.py
@@ -2,11 +2,20 @@
 Admin config for UserInfo app.
 """
 from common.utils import register_model_admins
+from django.contrib import admin
 
 from .models import PledgeClass, UserInfo
 
+class UserInfoAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name']
+    list_display = tuple([
+            field.name
+            for field in UserInfo._meta.fields
+            if field.name not in UserInfo.admin_display_excluded_fields
+        ])
+
+admin.site.register(UserInfo, UserInfoAdmin)
 
 register_model_admins(
     PledgeClass,
-    UserInfo,
 )
