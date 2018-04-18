835321d27b3084cb06720962991991d7c908318f
Thomas Schweich <thomas.schweich@gmail.com>
2018-04-18 14:39:41 -0400

    Add model forms

---
+++ sigmapiweb/apps/PubSite/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from sigmapiweb.apps.PubSite.models import Article, ArticleBlock, PublicImage, CarouselSlide
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['friendly_name', 'url_name', 'background_img']
+
+
+class ArticleBlockForm(ModelForm):
+    class Meta:
+        model = ArticleBlock
+        fields = ['page', 'title', 'anchor_id', 'body']
+
+
+class PublicImageForm(ModelForm):
+    class Meta:
+        model = PublicImage
+        fields = ['path']
+
+
+class CarouselSlideForm(ModelForm):
+    class Meta:
+        model = CarouselSlide
+        fields = ['image', 'title', 'description', 'precedence']
+
