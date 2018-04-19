fb561d979d9bcf2f624eeef0f263237674ae25ba
Jeff Martin <jamartin@wpi.edu>
2018-04-19 15:28:39 -0400

    dataTables are sortable (Added Icon to Make it Obvious to the user).

--- sigmapiweb/static/js/secure/autotable.js
+++ sigmapiweb/static/js/secure/autotable.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// Initialize all datatable class elements to data table.
+	// Initialize all datatable class elements to data table. (Used to Style the Data Table)
 	$.fn.dataTableExt.oStdClasses["sFilter"] = "dataTable-filter"
 	$.fn.dataTableExt.oStdClasses["sLength"] = "dataTable-length"
 	$.fn.dataTableExt.oStdClasses["sInfo"] = "dataTable-message"
@@ -7,6 +7,9 @@ $(document).ready(function() {
 	$.fn.dataTableExt.oStdClasses["sPagePrevDisabled"] = "btn btn-primary disabled dataTable-button-left"
 	$.fn.dataTableExt.oStdClasses["sPageNextEnabled"] = "btn btn-primary dataTable-button-right"
 	$.fn.dataTableExt.oStdClasses["sPageNextDisabled"] = "btn btn-primary disabled dataTable-button-right"
+	$.fn.dataTableExt.oStdClasses["sSortAsc"] = "dataTable-sort-asc"
+	$.fn.dataTableExt.oStdClasses["sSortDesc"] = "dataTable-sort-desc"
+
 	
 	// Initialize all data table instances.
 	$(".dataTable").dataTable();
