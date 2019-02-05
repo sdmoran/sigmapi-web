5d2089044a5a521ee47ef5f20120bbf3e155935c
Colin Willoughby <colinjw3@gmail.com>
2019-02-05 13:22:44 -0500

    Fix 'sigmapiweb' module import error

--- sigmapiweb/common/settings/base.py
+++ sigmapiweb/common/settings/base.py
@@ -63,7 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'sigmapiweb.apps.PubSite.processors.menu_items'
+                'apps.PubSite.processors.menu_items'
             ],
         },
     }
