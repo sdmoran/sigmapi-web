a5e1722c0a18d9c4466484b6eec579ccbaa5eb2d
Thomas Schweich <thomas.schweich@gmail.com>
2019-04-01 15:06:23 -0400

    Make Changes to `devl.py` and our documentation (#133)
    * Fix console spamming due to keyboard interrupts
    
    * Create tutorial script. Haven't tested the auto-pip-downloader
    
    * Change setup guide to reflect new devl.py script
    
    * Fix urllib error handling
    
    * Get auto pip installs working and make the setup process semi-optional
    
    * Delete dev.py
    
    This no longer serves a purpose
    
    * Delete Makefile
    
    Probably don't need to keep both methods of dev setup--it would just cause confusion.
    
    * Clarify necessity of C/C++ compiler, fix venv instructions for Windows
    
    * Add auto-generated help text for commands

--- docs/dev-guide/setup.md
+++ docs/dev-guide/setup.md
@@ -8,44 +8,17 @@
   The latest version is recommended.
 * [Python 3.6.*](https://www.python.org/downloads/): The primary language
   used in the development of the website.
-  * [Library: venv](https://docs.python.org/3.6/library/venv.html): Used to
+  * [Library: venv](https://docs.python.org/3.6/library/venv.html) [optional]: Used to
     create a virtual environment to isolate the development python from the
     system-wide python.
+* Python libraries as described in [`requirements/base.txt`](https://github.com/sigmapi-gammaiota/sigmapi-web/blob/master/sigmapiweb/requirements/base.txt) and, for developers, [`requirements/dev.txt`](https://github.com/sigmapi-gammaiota/sigmapi-web/blob/master/sigmapiweb/requirements/base.txt). These are installed automatically by the `devl.py` script.
+  * Several of these libraries require a C/C++ compiler. If you are using Windows, you may need to install one. The easiest way is to either use WSL (described below) or install Visual Studio Dev Tools from https://visualstudio.microsoft.com/downloads/.
+
+This guide will use `python3.6` to denote the Python interpreter on the command line. Note that it may be aliased as `python`, `python3`, `py`, or some other name on your system. If this is the case, replace `python3.6` in commands below with the alias of your interpreter.
 
 ### Additional Dependencies for Windows Developers
 
-Developers using Windows should consider using the Windows Subsystem for Linux. You can find
-the installation and setup documentation [here](https://docs.microsoft.com/en-us/windows/wsl/install-win10).
-Choosing Ubuntu as your subsystem is recommended if you're new to Linux.
-
-After installing the subsystem, you can access the Linux bash by opening a
-command prompt or PowerShell and entering:
-```bash
-> bash
-```
-
-Run the following to update your packages and install all development dependencies:
-```bash
-$ sudo apt update && sudo apt upgrade
-$ sudo add-apt-repository ppa:deadsnakes/ppa
-$ sudo apt update
-$ sudo apt-get install python3.6 python3-pip python3.6-dev python3.6-venv
-```
-
-In some cases, the subsystem will refuse to recognize Python 3.6 as the correct version
-when you run `$ python3 -V`. This can be solved with Ubuntu's update-alternatives, which allows
-you to select which version of Python 3 the `$ python3` command references.
-```bash
-$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-$ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-$ python3 get-pip.py --force-reinstall
-```
-You can then toggle between each version of python with the following command. Beware: you may
-have to re-install pip with the final two commands above each time you switch.
-```bash
-$ sudo update-alternatives --config python3
-```
+If you are using a Windows computer, it is recommended that you use the Windows Subsystem for Linux. For instructions on setting up this environment, see [Appendix: WSL Setup](#appendix-wsl-setup). After completing the instructions, follow the steps outlined in [First Time Setup](#first-time-setup).
 
 ## First Time Setup
 
@@ -66,9 +39,18 @@ $ cd sigmapi-web
 ```
 
 
-### 2. Create and activate the Python virtual environment.
+### 2. [Optional] Create and activate the Python virtual environment.
 
-This creates a local python virtualenv and activates it.
+This creates a local python virtualenv and activates it. This is recommended in order to isolate this Python environment from any others which are on your computer currently or may be installed in the future.
+
+Windows:
+
+```cmd
+> python3.6 -m venv .\venv
+> venv\bin\activate
+```
+
+Linux/Mac/WSL/Other Systems Using Bash:
 
 ```bash
 $ python3.6 -m venv ./venv
@@ -77,31 +59,49 @@ $ source venv/bin/activate
 
 ### 3. Set up the development environment
 
-You will only have to do this once. This installs requirements, loads initial
-data for the database, and collects static files.
+Run the `devl.py` script. The script will detect that it has not yet been set up and will attempt to automatically determine what variables are needed for your system. After it has done so, it will place them in a file called `devl_settings.json` which you can modify to your liking. It will also guide you through the completion of your development environment setup if necessary. After doing this, answering `y` or simply pressing `ENTER` when asked if you would like to run `dev` will install the Python package requirements, load initial data for the database, and collect static files.
 
 ```bash
 $ cd sigmapiweb
-$ make dev
+$ python3.6 devl.py
+...
+Would you like to run `dev` now to install your environment?
+> y
 ```
 
-### 5. Run Django.
+Note that the tools `devl.py` uses can be changed at any time in the future by modifying the variables placed in `devl_settings.json` after this step.
+
+### 4. Run Django.
+
+This step is handled by `devl.py`. You can use this command any time in the future while developing to view changes to the site locally.
 
 ```bash
-$ make run
+$ python3.6 devl.py run
 ```
 
-### 6. Open a web browser on your computer, and navigate to localhost:8000 to view the site.
+### 5. Open a web browser on your computer, and navigate to localhost:8000 to view the site.
 
-You can make changes to the code and your running instance will be updated automatically. You can log into the site with the admin account credentials you created earlier.  **NOTE:** Though changes will automatically update, JS and CSS resources will get cached and require a redownload using `ctrl-F5`
+You can make changes to the code and your running instance will be updated automatically. You can log into the site with the admin account credentials you created earlier.  **NOTE:** Though changes will automatically update, JS and CSS resources will get cached and require a redownload using `ctrl-F5`.
 
-When you're done, you can kill the server with `ctrl-c`. Then, deactivate the python virtualenv with:
+When you're done, you can kill the server with `ctrl-c`. 
+
+If you are using python virtualenv, deactivate it with:
 
 ```bash
 $ deactivate
 ```
 
+## Using the Database
+
+You should familiarize yourself with the database via the local [Admin Panel](localhost:8000/admin) on your local site. It is available anytime you run the server (use `python3.6 devl.py run`).
+
 ## Fixture Data
+
+Sample database entries are provided to aid in development and testing. They are automatically loaded by `python3.6 devl.py dev`, or you can load/reload them manually using:
+
+```bash
+$ python3.6 devl.py loaddata
+```
 
 When you load the dev fixture data, the following users are created (along with their associated groups):
 
@@ -121,20 +121,64 @@ philanthropychair:philanthropychair # The philanthropy chair.
 rushchair:rushchair # The rush chair.
 sage:sage # The sage.
 scholarshipchair:scholarshipchair # The scholarship chair.
-socialchair:socialchair # The social chair
+socialchair:socialchair # The social chair.
 steward:steward # The steward.
 ```
+
+These accounts will be modifiable from the Admin Panel in your local site.
 
 ### Updating Fixture Data
 
 ```bash
-$ make dumpdata
+$ python3.6 devl.py dumpdata
 ```
 
-This runs the following command:
+This approximates the following bash command:
+
 ```bash
 python3 manage.py dumpdata --natural-foreign -e contenttypes -e auth.Permission > fixtures/dev_data.json
 ```
+
 > --natural-foreign will use a more durable representation of foreign keys (ex. User.username instead of User.id)
 
 > -e flags are used to exclude contenttypes and permissions, which are already generated by syncdb
+
+### Appendix: WSL Setup
+
+Developers using Windows 10 should consider using the Windows Subsystem for Linux for site development. You can find
+the installation and setup documentation [here](https://docs.microsoft.com/en-us/windows/wsl/install-win10).
+Choosing Ubuntu as your subsystem is recommended if you're new to Linux.
+
+After installing the subsystem, you can access the Linux bash by opening a
+command prompt or PowerShell and entering:
+```bash
+> bash
+```
+
+Run the following to update your packages and install all development dependencies:
+```bash
+$ sudo apt update && sudo apt upgrade
+$ sudo add-apt-repository ppa:deadsnakes/ppa
+$ sudo apt update
+$ sudo apt-get install python3.6 python3-pip python3.6-dev python3.6-venv
+```
+
+In some cases, the subsystem will refuse to recognize Python 3.6 as the correct version
+when you run `$ python3 -V`. This can be solved with Ubuntu's update-alternatives, which allows
+you to select which version of Python 3 the `$ python3` command references.
+
+```bash
+$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+$ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+$ python3 get-pip.py --force-reinstall
+```
+
+You can then toggle between each version of python with the following command. Beware: you may
+have to re-install pip with the final two commands above each time you switch.
+
+```bash
+$ sudo update-alternatives --config python3
+```
+
+Once you have completed the installations, use the WSL terminal to continue from [First Time Setup](#first-time-setup).
--- sigmapiweb/devl.py
+++ sigmapiweb/devl.py
@@ -1,5 +1,5 @@
 """ 
-devl.py -- The more cross-platform makefile substitute
+devl.py -- The more cross-platform makefile substitute with setup tools
 
 Usage
 =====
@@ -24,6 +24,7 @@ To add a new command, you should
   - The key is the name of the command
   - The value is list containing other command names and/or functions which the command depends upon
 - Prefix any new command functions with 'r_' (for 'run') for consistency
+- Add a description of any new command functions using the `describe` decorator
 - Avoid pipes, redirects, or otherwise bash-y things for our Windows friends. Instead, just use multiple commands
   and rely on Python's baked-in io functionality. See r_dumpdata() and r_pylint() for examples.
 """
@@ -34,6 +35,8 @@ import json
 import os
 import glob
 
+# The values below are just defaults and will be superceded by devl_settings.json
+
 user_variables = {
     'PYTHON': 'python3.6',
     'PIP': 'sudo pip3.6',
@@ -42,21 +45,33 @@ user_variables = {
     'PORT': '8000',
 }
 
+if os.name == 'nt':
+    # We're in Windows, so we'll try different defaults
+    user_variables.update({
+        'PYTHON': 'python',
+        'PIP': 'pip',
+        'MANAGE': 'python manage.py'
+    })
+
+PROMPT = '\n{question}\n({options}) {default}\n> '
 help_commands_list = None  # Leave this empty, it is auto-populated
 
+
 # Helper Functions
+
 
 def vformat(space_separated):
     """ Format a string with global variables """
     return space_separated.format(**user_variables)
 
+
 def format_and_call(space_separated, **kwargs):
     """ Format global variables within ``space_separated`` and then subprocess.run the string, separated by spaces.
-    
+
     Passes ``kwargs`` to subprocess.run. Returns the output of subprocess.run. 
     Outputs the command executed similar to how make does it.
     Note this can be used even if there are no user_variable format specifiers.
-    
+
     Arguments:
         space_separated (str): Space-separated command to run
         kwargs: (dict): Dict to pass to subprocess.run as **kwargs
@@ -65,9 +80,10 @@ def format_and_call(space_separated, **kwargs):
     print(cmdstr)
     return subprocess.run(cmdstr.split(' '), **kwargs)
 
+
 def just_call(complete_arguments, **kwargs):
     """ Same functionality as subprocess.run, but prints the command it is executing ahead of time.
-    
+
     This function is different than format_and_call in that it doesn't attempt to substitute user_variables values
     and it takes a list of arguments, not a space-separated string. 
 
@@ -78,6 +94,7 @@ def just_call(complete_arguments, **kwargs):
     print(' '.join(complete_arguments))
     return subprocess.run(complete_arguments, **kwargs)
 
+  
 def glob_as_args(glob_str, **kwargs):
     """ Outputs a file glob as a verbose space-separated argument string for maximum crossplatforminess.
 
@@ -87,87 +104,297 @@ def glob_as_args(glob_str, **kwargs):
     """
     return ' '.join(glob.glob(glob_str, **kwargs))
 
+
+def prompt_user(question, options=('y', 'n'), case_sensitive=False, default_first=True, accept_any=False):
+    """ Prompts the user with the given question, providing them with the given options
+    Re-prompts the user until the response is in `options` unless accept_any=False
+    If options not provided, defaults to y/n.
+   Returns the user's input (in lowercase if case_sensitive=False)
+
+    Arguments:
+        question (str): The prompt for the user
+        options (iter(str)): (optional) The tuple/iterable of possible options to answer
+        case_sensitive (boolean): (optional) Whether or not the responses are case-sensitive
+        default_first (boolean): (optional) Whether or not an empty input should default to the first option
+        accept_any (boolean): Whether to check if the response is in the options. Automatically set to true if no options provided.
+    """
+    ops = ''
+    if options:
+        ops = '/'.join(options)
+    elif accept_any:
+        ops = 'any response accepted'
+    assert ops != '', 'prompt_user(): No options are provided, but kwarg accept_any is false. This is probably an error.'
+    default = ''
+    if options and default_first:
+        default = ' [{default}]'.format(default=options[0])
+    prompt = PROMPT.format(question=question, options=ops, default=default)
+    result = input(prompt)
+    if default_first and result == '':
+        result = options[0]
+    valid_inputs = options if case_sensitive else [o.lower() for o in options]
+    while (result if case_sensitive else result.lower()) not in valid_inputs and not accept_any:
+        print('Invalid option.')
+        result = input(prompt)
+    return result if case_sensitive else result.lower()
+
+
+def update_user_vars():
+    """ Updates `user_vars` with the contents of devl_settings.json """
+    with open('devl_settings.json', 'r') as settings_file:
+        try:
+            user_variables.update(json.load(settings_file))
+        except json.decoder.JSONDecodeError as e:
+            print('There is an error in your devl_setting.json file. Please fix it and try re-running your command.')
+            print('The output is as follows:')
+            print(e.msg)
+            should_regen = prompt_user(
+                'Would you like to just regenerate the file by running the setup script again?'
+                '\nThe command you tried to execute will not run upon completion, so you have time to change the defaults.'
+            )
+            if should_regen == 'y':
+                run('setup_devl')
+            else:
+                print('Okay, devl.py will now exit. Please fix the JSON file and try re-running the command.')
+                exit(0)
+
+def describe(description):
+    """ Decorator used to add descriptions to command functions. Descriptions should be succinct and written in the imperative
+    
+    Arguments:
+        description (str): A description of the actions the function takes
+    """
+    def wrapper(cmd_fn):
+        cmd_fn._help_description_ = description
+        return cmd_fn
+    return wrapper
+
+def description_of(cmd_str):
+    """ Determines the description of the given command by chaining together descriptions of all functions it ends up calling 
+    If a description cannot be found, uses the name of the function.
+    
+    Arguments:
+        cmd_str (str): The name of the command to generate a description of
+    """
+    descs = [description_of(c) if isinstance(c, str) else '\t\t* ' + getattr(c, '_help_description_', c.__name__) for c in commands[cmd_str]]
+    return '\n'.join(descs)
+
+# Command Function Helpers
+
+def download_pip():
+    """ Tries to use get-pip.py to download pip """
+    try:
+        from urllib import request
+        from urllib.error import URLError
+    except ImportError:
+        print('Unfortunately your python installation does not have urllib, so you will have to install pip manually.')
+        print('I\'d reccomend googling "how to install pip on [YOUR PLATFORM]"')
+        print('Once you have done so, re-run this script. You may have to restart your computer for the changes to take effect.')
+        print('devl.py will now exit.')
+        exit(0)
+    try:
+        get_pip = request.urlopen('https://bootstrap.pypa.io/get-pip.py').read()
+    except URLError:
+        print('There was an issue fetching get-pip.py. Are you connected to the internet?')
+        print('If not, connect to the internet and re-run this script after doing so.')
+        print('If you are connected yet this is not working, you will probably have to manually install pip.')
+        print('I\'d reccomend googling "how to install pip on [YOUR PLATFORM]" if that is the case.')
+        print('Once you have done so, re-run "python devl.py"')
+        print('devl.py will now exit.')
+        exit(0)
+    with open('get-pip.py', 'wb') as gp_file:
+        gp_file.write(get_pip)
+    print('Successfully downloaded get_pip.py')
+
 # Command Functions
+
+@describe('Install all Python requirements using pip')
 
 def r_requirements():
     devreqs = os.path.join('requirements', 'dev.txt')
     format_and_call('{PIP} install -r ' + devreqs)
 
+    
+@describe('Load fixture data to database')
 def r_loaddata():
     format_and_call('{MANAGE} loaddata {DEV_DATA}')
 
+
+@describe('Write current database as a fixture')
 def r_dumpdata():
     complete = format_and_call(
-        '{MANAGE} dumpdata --natural-foreign -e contenttypes -e auth.Permission', 
+        '{MANAGE} dumpdata --natural-foreign -e contenttypes -e auth.Permission',
         stdout=subprocess.PIPE
     )
     with open(user_variables['DEV_DATA'], 'w') as f:
         f.write(complete.stdout.text)
 
 
+@describe('Migrate database with --run-syncdb')
 def r_syncdb():
     format_and_call('{MANAGE}  migrate --run-syncdb')
 
+
+@describe('Collect static files')
 def r_static():
     format_and_call('{MANAGE} collectstatic --noinput')
 
+
+@describe('Compile SASS files, then run collectstatic')
 def r_static_prod():
     format_and_call('{MANAGE} compilecss')
     format_and_call('{MANAGE} collectstatic --noinput')
 
+@describe('Migrate the database')
 def r_migrate():
     format_and_call('{MANAGE} migrate')
 
+
+@describe('Run the dev server')
 def r_quickrun():
     format_and_call('{MANAGE} runserver 0.0.0.0:{PORT}')
 
+
+@describe('Run tests (not implemented)')
 def r_test():
     print('lol what tests')
 
+
+@describe('Run the pycodestyle checks')
 def r_pycodestyle():
     format_and_call('pycodestyle common --max-line-length=120')
     apps_files = glob_as_args('apps/*')
-    format_and_call("pycodestyle " + apps_files + " --exclude='apps/*/migrations/*' --max-line-length=120")
+    format_and_call("pycodestyle " + apps_files +
+                    " --exclude='apps/*/migrations/*' --max-line-length=120")
     scripts_path = os.path.abspath('../scripts')
     format_and_call('pycodestyle ' + scripts_path + ' --max-line-length=120')
 
+
+@describe('Run the pylint checks')
 def r_pylint():
     format_and_call('pylint common')
     apps_files = glob_as_args('apps/*')
     format_and_call('pylint ' + apps_files)
-    scripts = os.path.abspath('../scripts') 
+    scripts = os.path.abspath('../scripts')
     format_and_call('pylint ' + scripts + ' --max_line_length=120')
 
+
+@describe('Create new migrations based on changes to Models')
 def r_newmigrations():
     format_and_call('{MANAGE} makemigrations')
     print('Before you commit, remember to rename your migration with a description of what it does (but keep its numerical designator)')
     print('Example: Change "003_auto_20170101_1942.py" to something like "003_add_blacklist.py"')
 
+
+@describe('Create a local admin account')
 def r_superuser():
     format_and_call('{MANAGE} createsuperuser')
 
+
+@describe('Open the database shell prompt')
 def r_dbshell():
     format_and_call('{MANAGE} dbshell')
 
+
+@describe('Start an interactive python session with convenience imports')
 def r_shell():
     format_and_call('{MANAGE} shell')
 
+
+@describe('Destroy the Python environment (not implemented)')
 def r_destroyenv():
     print('Destructive commands are not yet implemented')
     # TODO Destroy the entire python environment based on requirements.txt like the Makefile did?
 
+@describe('Clean static files (not implemented)')
 def r_clean():
     print('Destructive commands are not yet implemented')
 
+
+@describe('Display help text')
 def r_help():
     print('devl.py -- The more cross-platform make-substitute for the Sigma Pi Gamma Iota website')
     print()
     print('Usage:')
     print('\t$ python devl.py COMMAND')
+    print()
     print('Available commands are:')
+    print()
     assert help_commands_list is not None, 'There was an issue populating the list of possible commands'
     for c in help_commands_list:
         print('\t' + c)
+        print(description_of(c))
+        print()
+
+
+@describe('Run the environment setup script')
+def r_setup_devl():
+    working_settings = {}
+    print('Welcome to devl.py -- The module that makes working with your dev environment suck a little less\n')
+    print('This setup script can be re-accessed at any time after it is complete using "python devl.py setup_devl"')
+    if not (sys.version_info > (3, 5)):
+        print(
+            'First things first, you need to install a newer version of python. '
+            'Head over to python.org/downloads and get the latest version for your system.'
+        )
+        print('Once you have done so, add it to your system path and re-run this script using the new version')
+        print('devl.py will now exit. See you soon!')
+        exit(0)
+    else:
+        print('Congratulations on running this script with a compatible version of python!')
+        working_settings['PYTHON'] = sys.executable
+    if not os.path.isfile('devl_settings.json'):
+        print('It has been detected that you have not yet set up devl.py. So let\'s do that!')
+    print('Let\'s see if you have pip installed...')
+    try:
+        import pip
+        print('Success!')
+    except ModuleNotFoundError:
+        print('It looks like this python installation does not have pip.')
+        should_install = prompt_user(
+            'Would you like devl.py to try to install pip for you?'
+            ' You should not use this option if you installed this python interpereter using a package manager such as apt.'
+            ' If you did, just answer "n" and then run "sudo apt install python3-pip" (Ubuntu) or whatever the equivalent on your system is.'
+            ' That will allow your pip installation to be managed by your package manager.'
+            ' If you are on Windows or Mac you should probably answer yes, unless you use a package manager such as choco or homebrew,'
+            ' and that package manager has pip3 available.'
+        )
+        if(should_install == 'y'):
+            download_pip()
+            print('Installing pip...')
+            if os.name == 'nt':
+                format_and_call(working_settings['PYTHON'] + ' get-pip.py')
+            else:
+                format_and_call('sudo ' + working_settings['PYTHON'] + ' get-pip.py')
+        else:
+            print('Okay, please install pip for this interpreter and re-run devl.py once you have done so.')
+            print('devl.py will now exit.')
+            exit(0)
+    if os.name == 'nt':
+        working_settings['PIP'] = working_settings['PYTHON'] + ' -m pip'
+    else:
+        working_settings['PIP'] = 'sudo ' + working_settings['PYTHON'] + ' -m pip'
+    working_settings['MANAGE'] = working_settings['PYTHON'] + ' manage.py'
+    user_variables.update(working_settings)
+    with open('devl_settings.json', 'w') as settings_file:
+        settings_file.write(json.dumps(user_variables, indent=4))
+    print('\%\%\%\% NOTICE \%\%\%\%')
+    print('A file called devl_settings.json has been placed in this directory. It contains the default settings determined from this setup process.')
+    print('If anything in devl.py doesn\'t work after this, you probably need to change something in that file.')
+    print('Since you have the proper python toolchain installed, we can now set up the dev environment.')
+    should_run_dev = prompt_user('Would you like to run `dev` now install your environment? (This can be done in the future using "python devl.py dev")')
+    if os.name == 'nt':
+        print('Since you are running Windows, you may need to install Visual Studio Dev Tools from https://visualstudio.microsoft.com/downloads/ in order to finish.')
+        print('We\'ll go ahead and try running dev anyways. If it fails, go to the above link and install "Tools for Visual Studio".')
+        print('Then, restart your computer, and then run "python devl.py dev"')
+    if should_run_dev == 'y':
+        run('dev')
+    else:
+        print('Setup is finished! Use "python devl.py dev" if you would like to install the environment in the future.')
+        exit(0)
+    print('Setup is finished! Check the output above to see if it succeeded.')
+    print('Use "python devl.py help" to see what else devl.py can do.')
+    print('devl.py will now exit.')
+    exit(0)  # Explicit exit in case we're here due to malformed JSON
 
 
 # Core Functionality
@@ -194,9 +421,11 @@ commands = {
     'destroyenv': ['clean', r_destroyenv],
     'clean': [r_clean],
     'help': [r_help],
+    'setup_devl': [r_setup_devl]
 }
 
 help_commands_list = sorted(list(commands.keys()))
+
 
 def run_command(command, executed_functions):
     """ Runs the given named command or function.
@@ -205,14 +434,19 @@ def run_command(command, executed_functions):
         command (str): The name of a command to run
         executed_functions (set): A set containing any command functions which have already been called
     """
-    reqs = commands[command]
+    try:
+        reqs = commands[command]
+    except KeyError:
+        sys.stderr.write('[devl.py] Invalid command "{0}"\n\n'.format(str(command)))
+        raise
     for r in reqs:
         try:
             if r not in executed_functions:
                 r()
                 executed_functions.add(r)
         except TypeError:
-            assert isinstance(r, str), 'Command list values must be callables or strings'
+            assert isinstance(
+                r, str), 'Command list values must be callables or strings'
             run_command(r, executed_functions)
         except KeyboardInterrupt:
             pass
@@ -224,7 +458,16 @@ def run(command):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        run(sys.argv[1])
+    if os.path.isfile('devl_settings.json'):
+        update_user_vars()
+        if len(sys.argv) == 2:
+            run(sys.argv[1])
+        else:
+            sys.stderr.write('[devl.py] Invalid argument format. Use "python devl.py help" for more information.\n')
     else:
-        print('Invalid argument format. Use "python devl.py help" for more information.')
+        if len(sys.argv) == 2:
+            sys.stderr.write('[devl.py] Notice -- devl.py has not yet been set up. Using default user variables. To fix this, run `python devl.py` with no arguments.\n\n')
+            run(sys.argv[1])
+        else:
+            run('setup_devl')
+
